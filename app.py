f91a26318f764e2001657f674a78a3386a2fc32f
tupapiloaleman <andreskammerath@gmail.com>
2020-06-02 13:07:46 -0300

    Just finished to comunicate the forms with the flask backend

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/hello", methods=['GET','POST'])
+@app.route("/")
 def index():
-	if request.method == 'GET':
-		return render_template("index.html")
+    return render_template("index.html")
 
-
+@app.route("/pepito", methods=['GET','POST'])
+def pepito():
+    if request.method == 'GET':
+        pass
+    else:
+        name = request.form.get("name")
+        mail = request.form.get("email")
+        content = request.form.get("message")
+        print(str(content))        
+    return redirect("/")
 if __name__ == '__main__':
     app.run()
