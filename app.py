a5dd014087b8d22ba5a9f29d89b3c85f9e54d4a3
tupapiloaleman <andreskammerath@gmail.com>
2020-06-01 23:36:18 -0300

    Incorporating form functionality

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route("/hello", methods=['GET','POST'])
 def index():
-	return render_template("index.html")
+	if request.method == 'GET':
+		return render_template("index.html")
 
 
 if __name__ == '__main__':
