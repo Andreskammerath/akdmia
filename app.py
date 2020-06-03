48638ffdcf115eb812aca873d73247c2a7216843
tupapiloaleman <andreskammerath@gmail.com>
2020-06-03 18:51:50 -0300

    Now I receive directly to my inbox messages sent throught the contact form

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 from flask import Flask, render_template, request, redirect
-
+from flask_mail import Mail, Message
+import os
 app = Flask(__name__)
+
+
 app.config['DEBUG'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/pepito", methods=['GET','POST'])
-def pepito():
+@app.route("/formSent", methods=['GET','POST'])
+def formSent():
+    #if receives get method, just redirect to "/"
     if request.method == 'GET':
         pass
+    #Else send the msg to mi inbox
     else:
         name = request.form.get("name")
-        mail = request.form.get("email")
+        email = request.form.get("email")
         content = request.form.get("message")
-        print(str(content))        
+        print(content)
+        msg = Message(content,sender="andreskammerath@gmail.com",
+                  recipients=["andreskammerath@gmail.com"])
+        mail.send(msg)
     return redirect("/")
 if __name__ == '__main__':
     app.run()
