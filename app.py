fd44b0771d5ede30d2e2073b3c1f3d5b32ee423e
tupapiloaleman <andreskammerath@gmail.com>
2020-06-05 21:35:18 -0300

    Finished layout.html to use as template for other pages

--- app.py
+++ app.py
@@ -32,5 +32,13 @@ def formSent():
                   recipients=["andreskammerath@gmail.com"])
         mail.send(msg)
     return redirect("/")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 if __name__ == '__main__':
     app.run()
