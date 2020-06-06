b767b388a2d666fc7f756ffe0a9bcf2ade7faa13
tupapiloaleman <andreskammerath@gmail.com>
2020-06-06 19:38:57 -0300

    I changed marketing text and finished the process to use layout with other pages of the site

--- app.py
+++ app.py
@@ -40,5 +40,10 @@ def services():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run()
