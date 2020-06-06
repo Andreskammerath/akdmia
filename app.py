a2e1f5205c33d7993eff104ed4c5744e2d970bac
tupapiloaleman <andreskammerath@gmail.com>
2020-06-06 20:12:57 -0300

    Now every message received through the website sends the content to mi personal inbox and sends an email to the message's sender

--- app.py
+++ app.py
@@ -28,9 +28,12 @@ def formSent():
         email = request.form.get("email")
         content = request.form.get("message")
         print(content)
-        msg = Message(subject="Mensaje desde akdemia.es",body=content,sender="andreskammerath@gmail.com",
+        msg = Message(subject="Mensaje desde akdemia.es",body=content + " - remitente: "+email ,sender="andreskammerath@gmail.com",
                   recipients=["andreskammerath@gmail.com"])
+        msg2 = Message(subject="Correo recibido en akdemia.es",body="Gracias por escribirnos. Muy pronto nos contactaremos.",sender="andreskammerath@gmail.com",
+                  recipients=[email])
         mail.send(msg)
+        mail.send(msg2)
     return redirect("/")
 
 @app.route("/services")
